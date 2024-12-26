b1dc2235f7d6ecbd4a3f498c03ceca603021bc6c
Anderson Kouadio <andersonkouadio0118@gmail.com>
2024-12-26 10:45:15 +0000

    mise a jour

--- components/dashboard/settings/restaurant-detail.tsx
+++ components/dashboard/settings/restaurant-detail.tsx
@@ -9,28 +9,27 @@ import '@smastrom/react-rating/style.css';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 
 import Link from 'next/link';
-import { Restaurant } from '@/types/models';
-
+import { Picture, Restaurant } from '@/types/models';
+import createUrlFile from '@/utils/createUrlFile';
 
 export default function RestaurantDetail({ restaurant }: { restaurant: Restaurant }) {
-
-  return (
+    return (
         <div className="w-full h-full p-4 flex flex-col items-start gap-2 justify-start">
             <div className="flex justify-center w-full mb-4">
-                <Avatar src={restaurant.logo} isBordered className="w-24 h-24 md:w-36 md:h-36" />
+                <Avatar src={createUrlFile(restaurant?.logo ?? '', 'restaurant') ?? ''} isBordered className="w-24 h-24 md:w-36 md:h-36" />
             </div>
             <h2 className={title({ size: 'h4', class: 'text-center' })}>{restaurant.nomEtablissement}</h2>
             <p className="text-sm text-gray-500 flex items-center gap-2">
                 <span className="font-semibold">Évaluation:</span> <Rating style={{ maxWidth: 150 }} value={4} readOnly />
             </p>
             <p className="text-sm text-gray-500">
-                <span className="font-semibold">Adresse:</span> {restaurant.commune}
+                <span className="font-semibold">Adresse:</span> {restaurant?.commune ?? ''}
             </p>
             <p className="text-sm text-gray-500">
-                <span className="font-semibold">Téléphone:</span> {restaurant.telephone}
+                <span className="font-semibold">Téléphone:</span> {restaurant?.telephone ?? ''}
             </p>
             <p className="text-sm text-gray-500">
-                <span className="font-semibold">Email:</span> {restaurant.email}
+                <span className="font-semibold">Email:</span> {restaurant?.email ?? ''}
             </p>
 
             <p className="text-sm text-gray-500 flex items-center gap-2">
@@ -43,18 +42,18 @@ export default function RestaurantDetail({ restaurant }: { restaurant: Restauran
                 <span className="font-semibold">Description:</span> {restaurant.description}
             </p>
 
-            {/* <div className="flex justify-center mt-10">{restaurant.pictures.length > 0 && <RestaurantImages images={restaurant.pictures} />}</div> */}
+            <div className="flex justify-center mt-10">{restaurant?.pictures && restaurant?.pictures?.length > 0 && <RestaurantImages images={restaurant.pictures} />}</div>
         </div>
     );
 }
 
-export function RestaurantImages({ images }: { images: string[] }) {
+export function RestaurantImages({ images }: { images: Picture[] }) {
     return (
         <Carousel className="w-8/12">
             <CarouselContent>
-                {images.map((image, index) => (
-                    <CarouselItem key={index}>
-                        <Image key={index} alt={`Image ${index}`} className="object-cover w-full h-full" src={image} isBlurred radius="sm" />
+                {images.map((picture) => (
+                    <CarouselItem key={picture?.id}>
+                        <Image alt={`Image ${picture?.id}`} className="object-cover w-full h-full" src={createUrlFile(picture.pictureUrl ?? '', 'restaurant')} isBlurred radius="sm" />
                     </CarouselItem>
                 ))}
             </CarouselContent>
