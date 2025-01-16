33c58b3b8596431f9cfe8698a42a880ab3e2d401
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-01-16 20:27:27 +0000

    mise a jour

--- app/(dashboard)/delivery/page.tsx
+++ app/(dashboard)/delivery/page.tsx
@@ -1,15 +1,13 @@
 import React, { Suspense } from 'react';
 import Content from './content';
 import Loading from '@/components/layouts/loading';
-import { getAllCourseExterne, getCourseExterne } from '@/src/actions/courses.actions';
+import { getAllCourseExterne } from '@/src/actions/courses.actions';
 import { auth } from '@/auth';
 
 export default async function DeliveryPage() {
     const session = await auth();
 
     const data = await getAllCourseExterne(session?.user?.restauranID ?? '');
-
-    const course1 = await getCourseExterne(data[3].id)
 
 
     return (
